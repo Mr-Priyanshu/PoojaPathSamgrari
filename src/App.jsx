3542d5e8dbd8e4d768ccbd7e74e1e6ba59c28dbe
Priyanshu Garg <impriyanshu.garg@gmail.com>
2024-08-21 21:18:01 +0530

    pooja path samgrari website subharmbh

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import logo from './assets/logo.png'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='ddiv'>
+        <img src="https://i.timesnowhindi.com/stories/ganesh_ji_sund_1200.jpg" alt="" className='ganeshji'/>
+
+      </div>
+        <img src={logo} alt="" className='thali'/>
+      
+    </>
+  )
+}
+
+export default App
